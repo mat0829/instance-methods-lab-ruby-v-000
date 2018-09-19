35a1b1237f88c12a8ba4f775e891c34ebfc52105
David Anderson <matandersonlv@hotmail.com>
2018-09-19 18:35:48 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,8 @@
 # Creating a new class called "Person"
 class Person
+  # Creating a method within my Person class called "talk"
+  def talk
+    # Defining what the method talk does
+    puts "Hello World!"
 
 end
